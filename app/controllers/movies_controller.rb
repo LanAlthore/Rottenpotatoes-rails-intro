4aa17540386be9d5e3c67de71e8ff461fd3b97b1
Alan Waite <alanmwaite@gmail.com>
2017-09-23 22:39:32 +0000

    Part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -44,14 +44,17 @@ class MoviesController < ApplicationController
     if @ratings and @sort_by
       case @sort_by
       when 'title'
-        @movies = Movie.where(rating: @ratings.keys).order('title ASC')
+        params[:ratings] ? @movies = Movie.where(rating: @ratings.keys).order('title ASC'):
+        @movies = Movie.all
         @title_hilite = 'hilite'
       when 'release'
-        @movies = Movie.where(rating: @ratings.keys).order('release_date ASC')
+        params[:ratings] ? @movies = Movie.where(rating: @ratings.keys).order('release_date ASC'):
+        @movies = Movie.all
         @release_hilite = 'hilite'
       end
     elsif @ratings
-      @movies = Movie.where(rating: @ratings.keys)
+      params[:ratings] ? @movies = Movie.where(rating: @ratings.keys):
+        @movies =Movie.all
     elsif @sort_by
       case @sort_by
         when 'title'
