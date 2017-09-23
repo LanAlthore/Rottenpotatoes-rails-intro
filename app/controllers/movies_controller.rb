527d587e2eb91f792c923a24d3c54fc990e4f345
Alan Waite <alanmwaite@gmail.com>
2017-09-23 16:31:47 +0000

    Part 2 complete try1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -25,7 +25,7 @@ class MoviesController < ApplicationController
       @release_hilite = 'hilite'
   else
     params[:ratings] ? @movies = Movie.where(rating: params[:ratings].key) :
-      movies =Movie.all
+      @movies =Movie.all
     end
 
   end
