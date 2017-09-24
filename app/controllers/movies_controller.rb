bc0f5f637ee9362d39f5b8fb387e63ade9d43069
Alan Waite <alanmwaite@gmail.com>
2017-09-24 02:57:49 +0000

    Part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
     @all_ratings=['G','PG','PG-13','R']
     @mark=['G','PG','PG-13','R']
     @ratings= params[:ratings]
-    
+    @movies = Movie.all
     
   end
 
